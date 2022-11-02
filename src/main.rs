2f81b4abe7813a68d567c9d0cd4497511b2ad45a
mogmoug <mogmoug123@outlook.com>
2022-11-02 02:23:57 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
 #![no_std] // 不链接Rust标准库
 #![no_main] // 禁用所有Rust层级的入口点
-
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// 这个函数将在panic时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello Rust!Hello Ferris!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
     loop {}
 }
